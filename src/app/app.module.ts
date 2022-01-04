a71a7667a4d19c8339b6942504357fef5d0aae35
unknown <diovanlucas0@gmail.com>
2022-01-04 16:12:26 -0300

    Contador

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { CounterComponent } from './counter/counter.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    CounterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+ }
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+
+})
+
+export class CounterComponent implements OnInit {
+  count:number=0
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  increment():void{
+    this.count += this.count <20? 1:0;
+
+  }
+  decrement():void{
+    this.count -= this.count <20? 1:0;
+  }
+
+
+  }
+
